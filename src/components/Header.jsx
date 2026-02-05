e44ee47550154db0075b927a7123e51d530a773e
ruzica-glavas <ruzi99@hotmail.it>
2026-02-05 15:06:53 +0100

    add: color of background and add titles for sections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 export default function Header(){
     return(
         <>
-            <nav className="w-full bg-white border-b border-gray-200">
-                <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between hover:text-black cursor-pointer">
-                    <span className="text-xl font-bold">MySite</span>
+            <nav className="navbar w-full bg-white border-b border-gray-200">
+                <div className="max-w-7xl mx-auto px-6 h-16 flex items-center justify-between hover:text-black">
+                    <span className="text-xl font-bold cursor-pointer">MySite</span>
                     <ul className="flex gap-6 text-gray-600">
                         <li className="hover:text-black cursor-pointer">Home</li>
                     </ul>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,30 @@
 export default function HomePage(){
     return(
         <>
+        <div>
             <h1 className="text-3xl font-bold p-6">
-                Home page
+                Hero
             </h1>
+        </div>
+
+        <div>
+            <h1 className="text-3xl font-bold p-6">
+                About
+            </h1>
+        </div>
+
+        <div>
+            <h1 className="text-3xl font-bold p-6">
+                Projects
+            </h1>
+        </div>
+
+        <div>
+            <h1 className="text-3xl font-bold p-6">
+                Contact
+            </h1>
+        </div>
+
         </>
     )
 }
